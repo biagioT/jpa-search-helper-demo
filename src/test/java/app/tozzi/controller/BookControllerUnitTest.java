33b2604f6b7676e22a49767f99369de72f59a862
biagio <biagio.tozzi@brt.it>
2025-02-02 15:17:49 +0100

    jpa-search-helper: bump to 3.4.0

--- src/test/java/app/tozzi/controller/BookControllerUnitTest.java
+++ src/test/java/app/tozzi/controller/BookControllerUnitTest.java
@@ -9,8 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -30,7 +30,7 @@ public class BookControllerUnitTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
+    @MockitoBean
     private BookManager bookManager;
 
     private static final String PATH = "/books";
--- src/test/java/app/tozzi/controller/GraphQLTest.java
+++ src/test/java/app/tozzi/controller/GraphQLTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -26,10 +26,10 @@ public class GraphQLTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private BookDataFetcher bookDataFetcher;
 
-    @MockBean
+    @MockitoBean
     private BookRepository bookRepository;
 
     @Test
